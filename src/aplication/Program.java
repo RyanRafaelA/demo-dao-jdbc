d21c77eb7000d3520623e82fd7a5a8aa7404341f
Ryan Rafael <ryrafael461@gmail.com>
2023-08-23 20:31:19 -0300

    Criado a classe do departamento

---
+++ src/aplication/Program.java
@@ -0,0 +1,11 @@
+package aplication;
+
+import model.entities.Departamento;
+
+public class Program {
+	public static void main(String[] args) {
+		Departamento obj = new Departamento(1, "books");
+		
+		System.out.println(obj);
+	}
+}
